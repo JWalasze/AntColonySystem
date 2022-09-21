e617fc43bf5957a1694ceb8a97c5ed5eff9b8f99
Jakub Walaszek <jakub.walaszek@stazysta.comarch.pl>
2022-09-21 17:58:08 +0200

    INFO

--- src/Graph/EdgeWithDestinationCity.cs
+++ src/Graph/EdgeWithDestinationCity.cs
@@ -2,7 +2,7 @@ namespace AntColonyNamespace
 {
     internal class EdgeWithDestinationCity : Edge
     {
-        public int DestinationCity { get; private set; }
+        public int DestinationCity { get; private set; } //DAC ze jak aktualizujemy to wtedy logika zeby obydwie krawedzie zrobilo
 
         public EdgeWithDestinationCity(Edge Edge, int DestCity)
             : base(Edge.Distance, Edge.InitialPheromoneLevel)
